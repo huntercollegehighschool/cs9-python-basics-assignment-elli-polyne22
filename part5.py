d46dc6a42e2309552da49103f117e3ccdad35e04
elli-polyne22 <104437128+elli-polyne22@users.noreply.github.com>
2022-05-02 17:27:54 -0400

    Update part5.py

--- part5.py
+++ part5.py
@@ -13,3 +13,7 @@ Hello, Ash Ketchum
 '''
 
 #start writing your code below
+firstname = int(input(" What is your first name?"))
+last name +omt(input("What is your last name?"))
+print("Hello")
+print first name, lastname
